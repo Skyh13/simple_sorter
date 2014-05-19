aa80bf4c0664fea9d1ae6fe38f53200b479e771e
Grant Furgiuele <grant.furgiuele@gmail.com>
2014-05-18 22:57:15 -0500

    Initial Commit

---
+++ sorter/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  sorter
+//
+//  Created by Grant Furgiuele on 5/18/14.
+//  Copyright (c) 2014 GrantFurgiuele. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
